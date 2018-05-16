8de434c6bbd0a457d63e069a28856eb1aea58a4d
James Staub <james@courbanize.com>
2018-05-15 22:00:00 -0400

    remove sequencer

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,30 +1,23 @@
 import Ember from 'ember';
 
-
 const { get, set, getOwner } = Ember;
 
 export default Ember.Component.extend({
-
   tagName: 'nav',
 
   didRender() {
-    let currentRoute = getOwner(this)
-      .lookup('controller:application')
+    let currentRoute = getOwner(this).lookup('controller:application')
       .currentPath;
-
 
     get(this, 'routes').map(route => {
       let isCurrent = route.route == currentRoute;
       set(route, 'isCurrent', isCurrent);
       return route;
-    })
-
+    });
   },
 
   routes: [
-    {route: 'index', display: 'home'},
-    {route: 'doodles.tone-lattice', display: 'tone lattice'},
-    {route: 'doodles.sequencer', display: 'sequencer'},
-  ],
-
+    { route: 'index', display: 'home' },
+    { route: 'doodles.tone-lattice', display: 'tone lattice' }
+  ]
 });
--- app/components/step-param-row.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  tagName: 'tr',
-  classNames: ['step-param-row'],
-});
